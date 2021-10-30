cd65ba22cbbf2472823f0ff85de8817a0695735f
Kalpesh-07 <64299042+Kalpesh-07@users.noreply.github.com>
2021-10-30 19:34:37 +0530

    Did Formatting

--- src/GlobalContext.js
+++ src/GlobalContext.js
@@ -3,7 +3,6 @@ import {createContext, useState} from "react";
 export const TodoContext = createContext()
 function GlobalContext({ children }) {
     const [ todo, setTodo ] = useState([])
-
     return (
         <TodoContext.Provider value={{ todo, setTodo }}>
             {children}
